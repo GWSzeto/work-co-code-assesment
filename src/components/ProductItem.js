bfe6f67b80d95296926b55399da196b8e3f3c3bf
gwszeto <sszeto@uwaterloo.ca>
2019-03-18 23:27:34 -0400

    Added styled components library and set to previous version for react compatibility

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Product from './Product'
+import styled from 'styled-components'
+
+const Button = styled.button`
+  background-color: #5e96d1;
+`
 
 const ProductItem = ({ product, onAddToCartClicked }) => (
   <div style={{ marginBottom: 20 }}>
@@ -8,11 +13,11 @@ const ProductItem = ({ product, onAddToCartClicked }) => (
       title={product.title}
       price={product.price}
       inventory={product.inventory} />
-    <button
+    <Button
       onClick={onAddToCartClicked}
       disabled={product.inventory > 0 ? '' : 'disabled'}>
       {product.inventory > 0 ? 'Add to cart' : 'Sold Out'}
-    </button>
+    </Button>
   </div>
 )
 
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import ProductsContainer from './ProductsContainer'
 import CartContainer from './CartContainer'
+import styled from 'styled-components'
+
+const StyledProductsContainer = styled(ProductsContainer)`
+  display: flex;
+  flex-wrap: wrap;
+`
 
 const App = () => (
   <div>
-    <h2>Shopping Cart Example</h2>
+    <h2>Acme</h2>
     <hr/>
     <ProductsContainer />
     <hr/>
